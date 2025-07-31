bb5b75da6bd1d4765ed75012fd88bd68af251836
swaminathan31 <swaminathan@padmasini.com>
2025-07-31 14:59:52 +0530

    first commit

--- backendSwami/routes/register.js
+++ backendSwami/routes/register.js
@@ -1,3 +1,60 @@
+// const express = require('express');
+// const multer = require('multer');
+// const userSchema = require('../models/User');
+// const router = express.Router();
+// const path = require('path');
+// const getConnection = require('../utils/dbConnection');
+// // multer setup for file uploads
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'uploads/');
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//     cb(null, uniqueSuffix + '-' + file.originalname);
+//   }
+// });
+// const upload = multer({ storage: storage });
+// //adding new user
+// router.post('/newUser', upload.single('photo'), async (req, res) => {
+//   console.log("inside register user")
+//   console.log("req.file:", req.file);
+//   try {
+//     const {
+//       firstname, lastname, email, password,
+//       mobile, dob, gender, selectedCourse, selectedStandard
+//     } = req.body;
+//  const dbName = "studentUsers" // 👈 e.g., jee, neet, etc.
+//     const connection = await getConnection(dbName);
+//     const User = connection.model('studentUserDetail', userSchema,'studentUserDetail'); 
+//       let photoUrl = null;
+//     if (req.file) {
+//       // photoUrl = `http://localhost:3000/uploads/${req.file.filename}`;
+//       photoUrl = `https://studentpadmasini.onrender.com/uploads/${req.file.filename}`;
+//     }
+
+//     const newUser = new User({
+//       firstname,
+//       lastname,
+//       email,
+//       password,
+//       mobile,
+//       dob,
+//       gender,
+//       selectedCourse,
+//       selectedStandard,
+//       photo:photoUrl
+//     });
+
+//     await newUser.save();
+//     res.status(201).json({ message: "User registered successfully" });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ error: "Registration failed" });
+//   }
+// });
+
+// module.exports = router;
 const express = require('express');
 const multer = require('multer');
 const userSchema = require('../models/User');
@@ -5,20 +62,22 @@ const router = express.Router();
 const path = require('path');
 const getConnection = require('../utils/dbConnection');
 // multer setup for file uploads
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, uniqueSuffix + '-' + file.originalname);
-  }
-});
-const upload = multer({ storage: storage });
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'uploads/');
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//     cb(null, uniqueSuffix + '-' + file.originalname);
+//   }
+// });
+//const upload = multer({ storage: storage });
 //adding new user
-router.post('/newUser', upload.single('photo'), async (req, res) => {
+//router.post('/newUser', upload.single('photo'), async (req, res) => {
+  router.post('/newUser', async (req, res) => {
   console.log("inside register user")
-  console.log("req.file:", req.file);
+  
+  //console.log("req.file:", req.file);
   try {
     const {
       firstname, lastname, email, password,
@@ -27,11 +86,10 @@ router.post('/newUser', upload.single('photo'), async (req, res) => {
  const dbName = "studentUsers" // 👈 e.g., jee, neet, etc.
     const connection = await getConnection(dbName);
     const User = connection.model('studentUserDetail', userSchema,'studentUserDetail'); 
-      let photoUrl = null;
-    if (req.file) {
-      // photoUrl = `http://localhost:3000/uploads/${req.file.filename}`;
-      photoUrl = `https://studentpadmasini.onrender.com/uploads/${req.file.filename}`;
-    }
+    //   let photoUrl = null;
+    // if (req.file) {
+    //   photoUrl = `https://studentpadmasini.onrender.com/uploads/${req.file.filename}`;
+    // }
 
     const newUser = new User({
       firstname,
